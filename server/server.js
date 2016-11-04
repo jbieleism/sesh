14b8861eaf835c259fdbc89cea397278b75d3e8d
Justin Biele <justin.biele@gmail.com>
2016-11-03 22:13:35 -0700

    [add] Add client and server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var path = require('path');
+
+var app = express();
+var port = 8000;
+
+mongoose.connect('mongodb://localhost:27017/seshdb')
+
+mongoose.connect('connection', function() {
+  console.log('Connected to database')
+})
+
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+
+app.listen(port, function(){
+  console.log('listening on port: ', port)
+})
