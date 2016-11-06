df48e5f9a2e7324be441d608b84e4e718c70e50d
Justin Biele <justin.biele@gmail.com>
2016-11-05 20:16:10 -0700

    [add] Add css and refactoring

--- client/app.js
+++ client/app.js
@@ -1,14 +1,5 @@
 (function() {
 
   angular.module('Sesh', ['ui.router'])
-    .config(function($stateProvider){
-      $stateProvider
-        .state('signUp', {
-          url: '/signup',
-          templateUrl: 'app/signup/signup.html',
-          controller: 'SignupController'
-        })
-
-    })
 
 }());
