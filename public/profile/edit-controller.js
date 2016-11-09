01fea82e37c9a6a0a4164287e6bcbf05df6feec8
Justin Biele <justin.biele@gmail.com>
2016-11-09 12:43:49 -0800

    [fix] Fix syntax error

--- public/profile/edit-controller.js
+++ public/profile/edit-controller.js
@@ -4,6 +4,34 @@
 
     .controller('EditProfileController', ['Upload', '$scope', '$state', '$http', function(Upload, $scope, $state, $http){
 
-    }])
+        $scope.user = JSON.parse(localStorage['User-Data'])
+
+        $scope.$watch(function(){
+          return $scope.file;
+        }, function(){
+          $scope.upload($scope.file);
+        });
+
+        $scope.upload = function(file) {
+          if (file){
+            Upload.upload({
+              url: '/api/profile/edit',
+              method: 'POST',
+              data: {userId: '$scope.user._id'},
+              file: file
+            })
+            .progress(function(e){
+              console.log('progress: ', e);
+            })
+            .success(function(data){
+              console.log('Success: ', data);
+            })
+            .error(function(error){
+              console.log('Grave error: ', error);
+            })
+          };
+        };
+
+    }]);
 
 }())
