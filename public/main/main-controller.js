5235b4682c87ab15c9db297008aa322c64b1af41
Justin Biele <justin.biele@gmail.com>
2016-11-12 22:00:52 -0800

    [edit] Edit main controller

--- public/main/main-controller.js
+++ public/main/main-controller.js
@@ -1,49 +1,39 @@
 (function(){
 
   angular.module('Sesh')
-    .controller('MainController', ['$scope', '$http', '$interval', function($scope, $http, $interval){
+    .controller('MainController', ['$scope', '$http', function($scope, $http){
 
 
 
       if (localStorage['User-Data'] !== undefined){
 
         $scope.user = JSON.parse(localStorage['User-Data']);
-        console.log("This is the user information: ", $scope.user)
+        console.log("This is the user information: ", $scope);
 
-      }
-
-
+      };
 
       $scope.sendSesh = function(){
 
-        console.log("poo poo")
 
-        // if (event.which === 13){
+        var request = {
+          user: $scope.user.username || $scope.user.email,
+          userId: $scope.user._id,
+          // userImage: $scope.user.image,
+          description: $scope.newSesh
+        }
 
-        //   var request = {
-        //     user: $scope.user.username || $scope.user.email,
-        //     userId: $scope.user._id,
-        //     userImage: $scope.user.image,
-        //     description: $scope.newSesh
-        //   };
+        $http.post('api/sesh/post', request)
+          .success(function(response){
+            console.log("Sesh posted from main controller");
 
-
-        //   $http.post('/api/sesh/post', request)
-        //     .success(function(response){
-        //       console.log("Sesh posted from main controller")
-        //     })
-        //     .error(function(error){
-        //       console.log("There has been a grave error: ", error)
-        //   });
-        // }
-      }
+          })
+          .error(function(error){
+            console.log("There has been a grave error: ", error);
+        });
+      };
 
 
 
-
-
-
-
-    }])
+    }]);
 
 }());
--- server/models/sesh.js
+++ server/models/sesh.js
@@ -1,11 +1,9 @@
 var mongoose = require('mongoose');
 
-module.exports = mongoose.model('Sesh', {
-  image: String,
-  user: String,
-  title: String,
-  location: String,
-  description: String,
-  date: {type: Date, default: Date.now}
-
+module.exports = mongoose.model('Waste', {
+    user: String,
+    userId: String,
+    userImage: String,
+    description: String,
+    date: {type: Date, default: Date.now}
 })
