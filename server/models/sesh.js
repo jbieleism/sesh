5febd9e6d8e3ad2f93af56ef583dc82c5b8dd864
Justin Biele <justin.biele@gmail.com>
2017-06-09 07:02:38 -0700

    [REMOVE] Remove and Restart

--- server/models/sesh.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('Sesh', {
-    user: String,
-    userId: String,
-    userImage: String,
-    description: String,
-    date: {type: Date, default: Date.now}
-})
--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('User', {
-  email: String,
-  username: String,
-  password: String,
-  image: String,
-  bio: String
-});
